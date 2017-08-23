5512e2706edd34088d3bcdd4f038d1884724ba23
kalmaske <kalmaske@uncc.edu>
2017-08-23 11:28:06 -0400

    ..

---
+++ ClozeCard.js
@@ -0,0 +1,7 @@
+var BasicCard = require('./basicCard');
+
+var cloze = function(text,cloze){
+    this.cloze = cloze;
+    this.partial = partial;
+    this.fullText = fullText;
+}
--- basicCard.js
+++ basicCard.js
@@ -1,6 +1,7 @@
-module.exports = function BasicCard(front, back) {
-  if (this instanceof BasicCard) {
-    this.front = front;
-    this.back = back;
-  } else return new BasicCard(front, back);
+function BasicCard(front, back) {
+  this.front = front;
+  this.back = back;
+
 };
+
+module.exports = BasicCard;
