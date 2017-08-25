908f9560ce829308ba6c54f2f498cacc7cb0b4e6
kalmaske <kalmaske@uncc.edu>
2017-08-24 21:10:09 -0400

    .

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,19 +1,17 @@
-function ClozeCard(text, cloze) {
-    var generator = cloze.replace(/[a-z0-9\s]/gi,".");
-    this.cloze = cloze;
-    this.fullText = fullText;
-    this.partial = text.replace(new regExp(cloze,"gi"),generator);
 
-    if (this.fullText.indexOf(this.cloze) === -1){
-        console.log(this.cloze + "is wrong , " + this.fullText +" is correct" );
+var ClozeCard = function (text, cloze) {
+    if (!(this instanceof ClozeCard)) {
+        return new ClozeCard(text, cloze);
     }
+
+    
+    var generator = cloze.replace(/[a-z0-9\s]/gi, '.');
+    this.fullText = fullText;
+    this.cloze = cloze;
+    this.partial = fullText.replace(new RegExp(cloze, "gi"), generator);
+    if (this.fullText.indexOf(this.cloze) === -1) {
+        console.log(this.cloze + "is wrong , " + this.fullText +" is correct");
+    }     
 };
 
-function ClozeCardPrototype() {
-
-  											
-};
-
-ClozeCard.prototype = new ClozeCardPrototype();
-
-module.exports = ClozeCard; 
+module.exports = ClozeCard;
